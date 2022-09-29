5d3c394d10a737010c1bc54c3b7acdd00f91fe8d
emanueledb <emanueledibene@gmail.com>
2022-09-29 10:03:27 +0200

    Modal created

---
+++ src/components/detailsModal/DetailsModal.vue
@@ -0,0 +1,51 @@
+<template>
+  <b-modal
+    :visible="showModal"
+    class="modal"
+    hide-header
+    size="lg"
+    hide-footer
+    no-close-on-backdrop
+  >
+    <b-button
+      type="button"
+      variant="light"
+      class="btn btn-sm modal__close"
+      @click="closeModal"
+      >&times;</b-button
+    >
+    <div class="flexcard">
+      <div class="flexcard__header p-3 border-bottom border-light">
+        <h3></h3>
+      </div>
+      <p></p>
+    </div>
+  </b-modal>
+</template>
+<script lang="ts">
+import Vue from "vue"
+import TvShow from "@/models/TvShow"
+
+export default Vue.extend({
+  name: "DetailsModal",
+  props: {
+    showModal: {
+      type: Boolean,
+      required: true,
+    },
+    tvShow: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    closeModal() {
+      this.$emit("close", false)
+    },
+  },
+})
+</script>
+
+<style lang="scss">
+@import "style";
+</style>
--- src/components/slider/Slider.vue
+++ src/components/slider/Slider.vue
@@ -4,22 +4,45 @@
       v-for="tvShow of group"
       :key="tvShow.id"
       class="container__slider__movie"
+      @click="sendTvShow(tvShow)"
     >
       <img :src="tvShow.image.original" alt="image-show" />
     </div>
+    <DetailsModal
+      :show-modal="showModal"
+      @close="closeModal"
+      :tv-show="tvShow"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue"
 import TvShow from "../../models/TvShow"
+import DetailsModal from "@/components/detailsModal/DetailsModal.vue"
 
 export default Vue.extend({
   name: "Slider",
+  components: { DetailsModal },
   props: {
     group: {
       type: Array as () => Array<TvShow>,
       required: true,
+    },
+  },
+  data() {
+    return {
+      showModal: false,
+      tvShow: {} as TvShow,
+    }
+  },
+  methods: {
+    closeModal(value: boolean) {
+      this.showModal = value
+    },
+    sendTvShow(tvShow: TvShow) {
+      this.tvShow = tvShow
+      this.showModal = true
     },
   },
 })
